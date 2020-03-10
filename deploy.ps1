d2bf6bc3770e57ab618430ce18ff8fc5612f69e2
Joan Arnaldich <jarnaldich@gmail.com>
2020-03-10 17:59:49 +0100

    Added deploy, solved error in deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -1,8 +1,12 @@
-("blog css images tags" -split " ") | % {
-    copy -Recurse .\_site\$_\*.* .\$_\
+copy -Recurse .\_site\blog\*\*\*\*.* .\blog\ 
+git add .\blog
+
+("css images tags" -split " ") | % {
+    copy -Recurse .\_site\$_\*.* .\$_\ 
     git add $_
 }
-copy .\_site\*.*ml .
+
+copy .\_site\*.*ml . 
 git add *.*ml
 git status
 echo "Check files before commiting and pushing..."
